7e792891e89058efbb12c00f41b5769e25827f1b
huang wei <1119765689hw@gmail.com>
2023-11-28 10:39:35 +0800

    init the experience module

---
+++ sections/Experience/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Experience = () => {
+  return (
+    <div className='my-64'>
+        <h2 className='text-8xl font-bold mb-32 w-full text-center'>Experience</h2>
+        <div className='w-[75%] mx-auto relative'>
+            <ul>
+                <li>
+123
+                </li>
+            </ul>
+        </div>
+    </div>
+  )
+}
+
+export default Experience
