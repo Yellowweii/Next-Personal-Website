040b459405e292659be9b2a61b4c06437d317e06
Wei  Huang <138266079+Yellowweii@users.noreply.github.com>
2023-11-29 20:28:40 +0800

    finish the responsive about module (#12)
    * finish the navbar module
    
    * finish the layout moule
    
    * finish the footer module
    
    * finish the about module
    
    * finish the skills module
    
    * init the experience module
    
    * finish the about module
    
    * finish the projects module
    
    * finish the article module
    
    * finish the responsive layout
    
    * finish the responsive footer
    
    * finish the responsive about module

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -8,12 +8,12 @@ import Experience from "@/sections/Experience";
 
 const About = () => {
   return (
-    <main className="flex flex-col w-full items-center justify-center dark:text-light">
-      <div className="p-24 pt-16 pb-0">
-        <AnimatedText className="!text-8xl mb-16" text="Passion Fuels Purpose!" />
-        <div className="grid w-full grid-cols-8 gap-16">
-          <div className="col-span-3 flex flex-col items-center justify-start">
-            <h2 className="mb-4 text-lg font-bold uppercase text-dark/75 dark:text-light/75">Biography</h2>
+    <main className="flex flex-col w-full items-center justify-center dark:text-light overflow-hidden">
+      <div className="p-8 sm:p-12 md:p-16 lg:p-24 pt-16 pb-0 w-full">
+        <AnimatedText className="!text-5xl sm:!text-7xl lg:!text-8xl mb-8 xl:mb-16" text="Passion Fuels Purpose!" />
+        <div className="grid w-full grid-cols-8 gap-8 sm:gap-16">
+          <div className="col-span-8 order-2 md:order-1 md:col-span-4 xl:col-span-3 flex flex-col items-center justify-start">
+            <h2 className="mb-4 text-lg font-bold uppercase text-dark/75 dark:text-light/75 self-start">Biography</h2>
             <p className="font-medium">
               Hi, I'm CodeBucks, a web developer and UI/UX designer with a passion for creating beautiful, functional, and user-centered digital experiences. With 4 years of experience in the field. I
               am always looking for new and innovative ways to bring my clients' visions to life.
@@ -26,28 +26,28 @@ const About = () => {
               to the opportunity to bring my skills and passion to your next project.
             </p>
           </div>
-          <div className="col-span-3 relative h-max rounded-2xl border-2 border-dark border-solid bg-light p-6 dark:bg-dark dark:border-light">
+          <div className="col-span-8 order-1 md:order-2 md:col-span-4 xl:col-span-3 relative h-max rounded-2xl border-2 border-dark border-solid bg-light p-6 dark:bg-dark dark:border-light">
             <div className="absolute top-0 -right-3 -z-10 w-[102%] h-[103%] rounded-[2rem] bg-dark dark:bg-light"></div>
-            <Image src={profilePic} alt="Yellowwei" className="w-full h-auto rounded-2xl" priority sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw" />
+            <Image src={profilePic} alt="Yellowwei" className="w-full h-auto rounded-2xl" priority />
           </div>
-          <div className="col-span-2 flex flex-col items-end justify-between">
-            <div className="flex flex-col items-end justify-center">
-              <span className="inline-block text-7xl font-bold">
+          <div className="col-span-8 order-3 xl:col-span-2 flex flex-row xl:flex-col items-center xl:items-end justify-between">
+            <div className="flex flex-col items-center xl:items-end justify-center">
+              <span className="inline-block text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold">
                 <AnimatedNumbers value={50} />+
               </span>
-              <h2 className="text-xl font-medium capitalize text-dark/75 dark:text-light/75">satisfied clients</h2>
+              <h2 className="text-base md:text-lg lg:text-xl font-medium capitalize text-dark/75 dark:text-light/75 text-center">satisfied clients</h2>
             </div>
-            <div className="flex flex-col items-end justify-center">
-              <span className="inline-block text-7xl font-bold">
+            <div className="flex flex-col items-center xl:items-end justify-center">
+              <span className="inline-block text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold">
                 <AnimatedNumbers value={40} />+
               </span>
-              <h2 className="text-xl font-medium capitalize text-dark/75 dark:text-light/75">projects completed</h2>
+              <h2 className="text-base md:text-lg lg:text-xl font-medium capitalize text-dark/75 dark:text-light/75 text-center">projects completed</h2>
             </div>
-            <div className="flex flex-col items-end justify-center">
-              <span className="inline-block text-7xl font-bold">
+            <div className="flex flex-col items-center xl:items-end justify-center">
+              <span className="inline-block text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold">
                 <AnimatedNumbers value={4} />+
               </span>
-              <h2 className="text-xl font-medium capitalize text-dark/75 dark:text-light/75">years of experience</h2>
+              <h2 className="text-base md:text-lg lg:text-xl font-medium capitalize text-dark/75 dark:text-light/75 text-center">years of experience</h2>
             </div>
           </div>
         </div>
--- components/AnimatedText/index.tsx
+++ components/AnimatedText/index.tsx
@@ -12,7 +12,7 @@ const AnimatedText = ({ text, className = "" }: AnimatedTextProps) => {
     <div className="w-full mx-auto py-2 flex items-center justify-center text-center">
       <h1 className={`inline-block w-full text-dark font-bold capitalize text-6xl ${className} dark:text-light`}>
         {text.split(" ").map((word, index) => (
-          <motion.span key={index} className={`inline-block ${className}`} initial={{ opacity: 0, y: 50 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 1, delay: index * 0.05 }}>
+          <motion.span key={index} className={`inline-block`} initial={{ opacity: 0, y: 50 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 1, delay: index * 0.05 }}>
             {word}&nbsp;
           </motion.span>
         ))}
