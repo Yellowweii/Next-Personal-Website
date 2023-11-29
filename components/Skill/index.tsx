1e9c1a68fde7a4867ff1a960e8f510937f7f2144
Wei  Huang <138266079+Yellowweii@users.noreply.github.com>
2023-11-29 21:05:16 +0800

    finish the responsive skills module (#13)
    * finish the navbar module
    
    * finish the layout moule
    
    * finish the footer module
    
    * finish the about module
    
    * finish the skills module
    
    * init the experience module
    
    * finish the about module
    
    * finish the projects module
    
    * finish the article module
    
    * finish the responsive layout
    
    * finish the responsive footer
    
    * finish the responsive about module
    
    * finish the responsive skills module

--- components/Skill/index.tsx
+++ components/Skill/index.tsx
@@ -12,7 +12,7 @@ interface SkillProps {
 const Skill = ({ name, x, y }: SkillProps) => {
   return (
     <motion.div
-      className="flex items-center justify-center rounded-full font-semibold bg-dark text-light py-3 px-6 shadow-dark cursor-pointer absolute dark:bg-light dark:text-dark"
+      className="flex items-center justify-center rounded-full font-semibold bg-dark text-light lg:py-3 lg:px-6 lg:text-lg shadow-dark cursor-pointer absolute dark:bg-light dark:text-dark text-sm py-1.5 px-3 md:py-2 md:px-4 md:text-base"
       whileHover={{ scale: 1.05 }}
       initial={{ x: 0, y: 0 }}
       whileInView={{ x: x, y: y, transition: { duration: 1.5 } }}
